c34245da5cabf52ad8e38bffdb7fbf788960d257
Diogo Duarte <diogo.duarte@vnc.biz>
2017-07-08 13:50:19 +0100

    [FIX] install script

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
+if [ ! -f '~/.bkodoorc']; then
+    touch ~/.bkodoorc 
+    echo "SERVER_NAME='$HOSTNAME-server'" >> ~/.bkodoorc
+    echo "DB_NAME='v10_dbname'" >> ~/.bkodoorc
+    echo "DATA_FOLDER='/opt/odoo/.local/share/Odoo'" >> ~/.bkodoorc
+    echo "S3_URL='s3://bucket_name/folder/'" >> ~/.bkodoorc
+    echo "SLACK_HOOK='https://hooks.slack.com/services/XX/ZZ'" >> ~/.bkodoorc
+fi
 
-touch ~/.bkodoorc 
-echo "SERVER_NAME='$HOSTNAME-server'" >> ~/.bkodoorc
-echo "DB_NAME='v10_dbname'" >> ~/.bkodoorc
-echo "DATA_FOLDER='/opt/odoo/.local/share/Odoo'" >> ~/.bkodoorc
-echo "S3_URL='s3://bucket_name/folder/'" >> ~/.bkodoorc
-echo "SLACK_HOOK='https://hooks.slack.com/services/XX/ZZ'" >> ~/.bkodoorc
-
-git clone https://github.com/diogocduarte/backup-odoo-s3.git .backup
+git clone https://github.com/diogocduarte/backup-odoo-s3.git ~/.backup
+chmod +x ~/.backup/back
